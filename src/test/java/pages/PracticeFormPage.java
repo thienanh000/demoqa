c3511af0d6b7199e55a3e72adb2ce056409a01e8
thienanh000 <nguyenlethienanh000@gmail.com>
2025-04-22 23:59:30 +0700

    commit

---
+++ src/test/java/pages/PracticeFormPage.java
@@ -0,0 +1,150 @@
+package pages;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class PracticeFormPage {
+    WebDriver driver;
+
+    @FindBy(id = "firstName")
+    private WebElement firstNameField;
+    @FindBy(id = "lastName")
+    private WebElement lastNameField;
+    @FindBy(id = "userEmail")
+    private WebElement emailField;
+    @FindBy(xpath = "//label[text()='Male']")
+    private WebElement maleGender;
+    @FindBy(xpath = "//label[text()='Female']")
+    private WebElement femaleGender;
+    @FindBy(xpath = "//label[text()='Other']")
+    private WebElement otherGender;
+    @FindBy(id = "userNumber")
+    private WebElement mobileField;
+    @FindBy(id = "dateOfBirthInput")
+    private WebElement dateOfBirthInput;
+    @FindBy(className = "react-datepicker__month-select")
+    private WebElement monthSelect;
+    @FindBy(className = "react-datepicker__year-select")
+    private WebElement yearSelect;
+    @FindBy(xpath = "//div[contains(@class, 'react-datepicker__day--0')]")
+    private WebElement daySelect;
+    @FindBy(id = "subjectsInput")
+    private WebElement subjectInput;
+    @FindBy(xpath = "//label[text()='Sports']")
+    private WebElement sportsHobby;
+    @FindBy(xpath = "//label[text()='Reading']")
+    private WebElement readingHobby;
+    @FindBy(xpath = "//label[text()='Music']")
+    private WebElement musicHobby;
+    @FindBy(id = "uploadPicture")
+    private WebElement uploadPictureButton;
+    @FindBy(id = "currentAddress")
+    private WebElement addressField;
+    @FindBy(id = "react-select-3-input")
+    private WebElement stateField;
+    @FindBy(id = "react-select-4-input")
+    private WebElement cityField;
+    @FindBy(id = "submit")
+    private WebElement submitButton;
+    @FindBy(id = "example-modal-sizes-title-lg")
+    private WebElement successModalTitle;
+    @FindBy(id = "example-modal-sizes-title-lg")
+    private List<WebElement> successModalTitleList;
+
+    public PracticeFormPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void enterFirstName(String firstName) {
+        firstNameField.sendKeys(firstName);
+    }
+
+    public void enterLastName(String lastName) {
+        lastNameField.sendKeys(lastName);
+    }
+
+    public void enterEmail(String email) {
+        emailField.sendKeys(email);
+    }
+
+    public void selectGender(String gender) {
+        switch (gender.toLowerCase()) {
+            case "male" -> maleGender.click();
+            case "female" -> femaleGender.click();
+            case "other" -> otherGender.click();
+        }
+    }
+
+    public void enterMobile(String mobile) {
+        mobileField.sendKeys(mobile);
+    }
+
+    public void selectDateOfBirth(String day, String month, String year) {
+        dateOfBirthInput.click();
+        new Select(monthSelect).selectByVisibleText(month);
+        new Select(yearSelect).selectByVisibleText(year);
+        driver.findElement(By.xpath("//div[contains(@class, 'react-datepicker__day--0" + day + "')]")).click();
+    }
+
+    public void enterSubject(String subject) {
+        subjectInput.sendKeys(subject);
+        subjectInput.sendKeys(Keys.ENTER);
+    }
+
+    public void selectHobby(String hobby) {
+        switch (hobby.toLowerCase()) {
+            case "sports" -> sportsHobby.click();
+            case "reading" -> readingHobby.click();
+            case "music" -> musicHobby.click();
+        }
+    }
+
+    public void uploadPicture(String filePath) {
+        uploadPictureButton.sendKeys(filePath);
+    }
+
+    public void enterAddress(String address) {
+        addressField.sendKeys(address);
+    }
+
+    public void selectState(String stateName) {
+        stateField.sendKeys(stateName);
+        stateField.sendKeys(Keys.ENTER);
+    }
+
+    public void selectCity(String cityName) {
+        cityField.sendKeys(cityName);
+        cityField.sendKeys(Keys.ENTER);
+    }
+
+    public void submitForm() {
+        submitButton.click();
+    }
+
+    public boolean isSuccessModalShown() {
+        return successModalTitleList.size() > 0;
+    }
+
+    public String getModalText() {
+        return successModalTitle.getText();
+    }
+
+    public String getMobileFieldBorderColor() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.attributeContains(mobileField, "border-color", "rgb(220, 53, 69"));
+        return mobileField.getCssValue("border-color");
+    }
+
+    public void removeAds() {
+        ((JavascriptExecutor) driver).executeScript("document.querySelector('footer')?.remove();");
+        ((JavascriptExecutor) driver).executeScript("document.getElementById('fixedban')?.remove();");
+    }
+}
---
+++ src/test/java/tests/PracticeFormTest.java
@@ -0,0 +1,74 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import pages.PracticeFormPage;
+
+public class PracticeFormTest {
+    WebDriver driver;
+    PracticeFormPage practiceFormPage;
+
+    @BeforeTest
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demoqa.com/automation-practice-form");
+        practiceFormPage = new PracticeFormPage(driver);
+    }
+
+    @Test
+    public void testFormSubmissionWithValidData() {
+
+        practiceFormPage.removeAds();
+        practiceFormPage.enterFirstName("Demo");
+        practiceFormPage.enterLastName("Qa");
+        practiceFormPage.enterEmail("tester123@example.com");
+        practiceFormPage.selectGender("Female");
+        practiceFormPage.enterMobile("0123456789");
+        practiceFormPage.selectDateOfBirth("15", "December", "1990");
+        practiceFormPage.enterSubject("Quality control engineer");
+        practiceFormPage.selectHobby("Music");
+        practiceFormPage.uploadPicture(System.getProperty("user.dir") + "\\src\\test\\resources\\sample.jpg");
+        practiceFormPage.enterAddress("123 First Street");
+        practiceFormPage.selectState("NCR");
+        practiceFormPage.selectCity("Delhi");
+        practiceFormPage.submitForm();
+
+        Assert.assertTrue(practiceFormPage.isSuccessModalShown(), "Submission modal should be displayed!");
+        Assert.assertEquals(practiceFormPage.getModalText(), "Thanks for submitting the form");
+    }
+
+    @Test
+    public void testFormSubmissionWithoutMobileNumber() {
+
+        driver.navigate().refresh();
+        practiceFormPage.removeAds();
+        practiceFormPage.enterFirstName("Demo");
+        practiceFormPage.enterLastName("Qa");
+        practiceFormPage.enterEmail("tester456@example.com");
+        practiceFormPage.selectGender("Male");
+        practiceFormPage.selectDateOfBirth("20", "July", "1992");
+        practiceFormPage.enterSubject("English");
+        practiceFormPage.selectHobby("Reading");
+        practiceFormPage.uploadPicture(System.getProperty("user.dir") + "\\src\\test\\resources\\sample.jpg");
+        practiceFormPage.enterAddress("456 Second Street");
+        practiceFormPage.selectState("Haryana");
+        practiceFormPage.selectCity("Karnal");
+        practiceFormPage.submitForm();
+
+        String borderColor = practiceFormPage.getMobileFieldBorderColor();
+        Assert.assertTrue(borderColor.contains("rgb(220, 53, 69"),
+                "Mobile number field should be marked as invalid!");
+
+        Assert.assertFalse(practiceFormPage.isSuccessModalShown(), "Practice Form should not be submitted without mobile number!");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
